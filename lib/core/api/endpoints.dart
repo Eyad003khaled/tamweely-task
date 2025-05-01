4e35af201e587c89bb41e9af43598f0b00182d26
Eyad khaled <143270409+Eyad003khaled@users.noreply.github.com>
2025-05-01 04:42:53 +0300

    Finishing Api Integration & handling Security

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Endpoints {
-  static String baseUrl = "https://run.mocky.io/v3/";
+  static String baseUrl = dotenv.env['API_BASE_URL']!;
   static String products = "cee5d5f0-968a-48e2-8003-70a93faa1f95";
   static String aboutCompany = "933c29f8-9ecc-4d5b-8475-b9410a1f8d7f";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tamweely_task/app/tamweely.dart';
 
 import 'core/database/cache/cache_helper.dart';
@@ -6,6 +7,7 @@ import 'core/services/injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(); 
   initGetIt();
   await getIt<CacheHelper>().init();
 
